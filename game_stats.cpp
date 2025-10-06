6c90490334d168b6c90dde47cd748a9cc39b87eb
sophianavarro <snavarr23@gmail.com>
2025-10-06 14:19:12 -0700

    Made change

--- game_stats.cpp
+++ game_stats.cpp
@@ -2,6 +2,8 @@
 // Oct 6 2025
 #include <iostream>
 using namespace std;
+void showDrive()
+
 int main() {
     const int SIZE = 10;
     cout << "Detroit Lions at Baltimore Ravens" << endl
@@ -10,5 +12,17 @@ int main() {
     int yards[SIZE] = { 67, 98, 60 , 94, 35, 25, 45, 50, 30, 40 };
     cout << "First drive yards: " << yards[0] << endl;
     cout << "Epic second drive : " << yards[1] << " yards" << endl;
+    showDrive();
+    showDrive();
+
+    cout << "Array memory: ~" << sizeof(yards) << " bytes" << endl;
+
+   
     return 0;
+}
+void showDrive()
+{
+    static int driveNum = 1; // Persists across function calls
+    cout << "Showing drive " << driveNum << endl;
+    driveNUm;
 }
