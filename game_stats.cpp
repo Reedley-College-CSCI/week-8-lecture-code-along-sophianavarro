0fce0e7467d298c263754fc59443c632e4ee839f
sophianavarro <snavarr23@gmail.com>
2025-10-06 14:24:49 -0700

    Added Loop to display all drive yards

--- game_stats.cpp
+++ game_stats.cpp
@@ -2,7 +2,7 @@
 // Oct 6 2025
 #include <iostream>
 using namespace std;
-void showDrive()
+void showDrive();
 
 int main() {
     const int SIZE = 10;
@@ -12,17 +12,21 @@ int main() {
     int yards[SIZE] = { 67, 98, 60 , 94, 35, 25, 45, 50, 30, 40 };
     cout << "First drive yards: " << yards[0] << endl;
     cout << "Epic second drive : " << yards[1] << " yards" << endl;
-    showDrive();
-    showDrive();
 
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Drive " << i + 1 << "; " << yards[1] << " yards" << endl;
+    }
+    showDrive();
+    showDrive();
+    // Display memory usage of array
     cout << "Array memory: ~" << sizeof(yards) << " bytes" << endl;
 
    
     return 0;
 }
-void showDrive()
+/* void showDrive()
 {
     static int driveNum = 1; // Persists across function calls
     cout << "Showing drive " << driveNum << endl;
     driveNUm;
-}
+}*/
